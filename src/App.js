8c459a463098221d04102b4993f6146999b95064
Faaiz Asif <k190138@nu.edu.pk>
2021-12-01 11:42:37 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Friend-Finity - Your doorway to infinite friends!
+    </div>
+  );
+}
+
+export default App;
