3e1c351ee345fec8be60c0d15198972a1cfd4db8
Faaiz Asif <k190138@nu.edu.pk>
2021-12-13 15:41:12 +0500

    frontend

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,11 +1,15 @@
 import "./post.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { PlusOneTwoTone } from "@mui/icons-material";
+import { useState } from "react";
 import {Users} from "../../dummyData"
 export default function Post({post}) {
-
-  const user =
-  console.log(post);
+  const [like,setLike] = useState(post.like)
+  const [isLiked,setIsLiked] = useState(false)
+  const likeHandler =()=>{
+    setLike(isLiked ? like-1 : like+1)
+    setIsLiked(!isLiked)
+  }
     return (
         <div className="post">
         <div className="postWrapper">
@@ -29,9 +33,8 @@ export default function Post({post}) {
           </div>
           <div className="postBottom">
             <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt=""/>
-            <img className ="heartIcon" src="assets/heart.png" alt=""/>
-            <button className="postlikeCounter">{post.like} People like it</button>
+            <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
+            <span className="postLikeCounter">{like} people like it</span>
             </div>
             <div className="postBottomRight">
             <button className="postCommentText">{post.comment} Comments</button>
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,20 +1,52 @@
 import "./rightbar.css"
 import Online from "../online/Online";
 import {Users} from "../../dummyData"
-export default function rightbar() {
-    return (
-        <div className="rightbar">
-            <div className="rightbarWrapper">
+export default function rightbar({profile}) {
+    const HomeRightbar =() =>{
+        return(
+            <>
+             <div className="birthdayContainer">
             <span className="birthdayText">
             <img className="birthdayImg" src="assets/gift.png" alt="" />
             <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
           </span>
+          </div>
           <h4 className="rightbarTitle">Online Friends</h4>
             <ul className="rightbarFriendList">
                 {Users.map(u=>(
                     <Online key={u.id} user={u}/>
                 ))}
-        </ul>
+            </ul> 
+            </>
+        )
+    };
+    const ProfileRightbar =() =>{
+        return(
+        <>
+        <h4 className="rightbarTitle">User Information</h4>
+        <div className="rightbarInfo">
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">City:</span>
+                <span className="rightbarInfoKey">Karachi</span>
+            </div>
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">From:</span>
+                <span className="rightbarInfoKey">Lahore</span>
+            </div>
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">Relationship:</span>
+                <span className="rightbarInfoKey">Single</span>
+            </div>
+
+        </div>
+        </>
+        )
+    }
+
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                <ProfileRightbar/>
             </div>
         </div>
     )
